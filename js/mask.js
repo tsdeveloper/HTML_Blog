2dddc4bf2da9105a258b46e084cd28e74569c054
Matheus Carneiro <matheus10carneiro@gmail.com>
2019-03-15 20:09:59 -0300

    First Commit

---
+++ js/mask.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    $('#phonenumber').mask('(00) 0000-0000'); //Phone number
+});
---
+++ js/table.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("tbody tr:not(.sub)").hide();
+
+    $("#clicktable").click(function() {
+        $(".view").show();
+    });
+
+    $("tbody tr:#clicktable").hide();
+
+});
